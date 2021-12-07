a6258427f1815284c6911451a24fefbaac17f56b
Adam-Bjurhede <adambjurhede@gmail.com>
2021-12-07 15:55:03 +0100

    part3 3.6 done

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let phonebook = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+];
+
+//Get all persons
+app.get('/api/persons', (request, response) => {
+	response.json(phonebook);
+});
+
+//Get one person
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+
+	const person = phonebook.find((person) => person.id === id);
+
+	if (!person) {
+		return response.status(404).json({ success: false, message: 'No person was found' });
+	}
+
+	response.status(200).json(person);
+});
+
+//Delete person
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+
+	phonebook = phonebook.filter((person) => person.id !== id);
+
+	response.status(204).end();
+});
+
+//Add new entrie
+app.post('/api/persons', (request, response) => {
+	const newPerson = request.body;
+
+	if (!newPerson.name || !newPerson.number) {
+		return response.status(400).json({ error: 'Both name and number has to be present' });
+	}
+
+	const personExist = phonebook.find((person) => person.name.toLowerCase() === newPerson.name.toLowerCase());
+
+	if (personExist) {
+		return response.status(400).json({ error: 'Name must be unique' });
+	}
+
+	newPerson.id = Math.round(Math.random() * 10000);
+
+	phonebook = phonebook.concat(newPerson);
+
+	response.status(200).json(newPerson);
+});
+
+//Get Info
+app.get('/info', (request, response) => {
+	const date = new Date();
+
+	response.send(`<p>Phonebook has info for ${phonebook.length} people</p>
+	<p>${date}</p>
+	
+	`);
+});
+
+//Port
+const PORT = 3001;
+//Start server
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
